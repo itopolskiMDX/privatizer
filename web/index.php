8552f3060150844ef8ba6bd5b693325a155cbc0f
itopolskiMDX <you@example.com>
2018-03-30 13:27:05 +0100

    Initial

--- web/index.php
+++ web/index.php
@@ -17,7 +17,7 @@ if(!isset($_SESSION['username'])) {
   } else {
     echo('<form method="POST" action="index.php">
     <input type="text" name="username" placeholder="Username"><br>
-    <input type="text" name="password" placeholder="Password"><br>
+    <input type="password" name="password" placeholder="Password"><br>
     <input type="submit" value="Sign in">
     </form>
     ');
