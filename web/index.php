15cfffc5882c243e427ec855dbce6c58dee83a01
itopolskiMDX <you@example.com>
2018-03-30 13:31:22 +0100

    Initial

--- web/index.php
+++ web/index.php
@@ -15,11 +15,44 @@ if(!isset($_SESSION['username'])) {
       }
     }
   } else {
-    echo('<form method="POST" action="index.php">
-    <input type="text" name="username" placeholder="Username"><br>
-    <input type="password" name="password" placeholder="Password"><br>
-    <input type="submit" value="Sign in">
-    </form>
+    echo('
+    <!DOCTYPE html>
+    <html>
+
+    <head>
+
+      <meta charset="UTF-8">
+
+      <title>CodePen - Log-in</title>
+
+      <link rel="stylesheet" href="http://codepen.io/assets/libs/fullpage/jquery-ui.css">
+
+        <link rel="stylesheet" href="css/style.css" media="screen" type="text/css" />
+
+    </head>
+
+    <body>
+
+      <div class="login-card">
+        <h1>Log-in</h1><br>
+      <form method="POST" action="index.php">
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="password" placeholder="Password">
+        <input type="submit" name="login" class="login login-submit" value="login">
+      </form>
+
+      <div class="login-help">
+        <a href="#">Register</a> • <a href="#">Forgot Password</a>
+      </div>
+    </div>
+
+    <!-- <div id="error"><img src="https://dl.dropboxusercontent.com/u/23299152/Delete-icon.png" /> Your caps-lock is on.</div> -->
+
+      <script src="http://codepen.io/assets/libs/fullpage/jquery_and_jqueryui.js"></script>
+
+    </body>
+
+    </html>
     ');
   }
 } else {
