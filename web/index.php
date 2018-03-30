a361ce43b22b661f41ce1f7ba04c3ed3e29a2ca4
itopolskiMDX <you@example.com>
2018-03-30 13:45:56 +0100

    Initial

--- web/index.php
+++ web/index.php
@@ -17,39 +17,34 @@ if(!isset($_SESSION['username'])) {
   } else {
     echo('
     <!DOCTYPE html>
-    <html>
+    <html lang="en" >
 
     <head>
-
       <meta charset="UTF-8">
+      <title>Login Form</title>
 
-      <title>Privatizer - Log-in</title>
-
-      <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.0/themes/smoothness/jquery-ui.css">
-
-        <link rel="stylesheet" href="css/style.css" media="screen" type="text/css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
 
     </head>
 
     <body>
 
-      <div class="login-card">
-        <center><img src="images/spy.png" style="width: 230px;"></center><br>
-        <h1>Log-in</h1><br>
-      <form method="POST" action="index.php">
-        <input type="text" name="username" placeholder="Username">
-        <input type="password" name="password" placeholder="Password">
-        <input type="submit" name="login" class="login login-submit" value="login">
-      </form>
-
-      <div class="login-help">
-        <a href="#">Register</a> • <a href="#">Forgot Password</a>
-      </div>
+      <div class="login">
+    	<h1>Login</h1>
+        <form method="POST" action="index.php">
+        	<input type="text" name="username" placeholder="Username" required="required" />
+            <input type="password" name="password" placeholder="Password" required="required" />
+            <button type="submit" class="btn btn-primary btn-block btn-large">Let me in.</button>
+        </form>
     </div>
 
-    <!-- <div id="error"><img src="https://dl.dropboxusercontent.com/u/23299152/Delete-icon.png" /> Your caps-lock is on.</div> -->
 
-      <script src="https://code.jquery.com/ui/1.12.0/jquery-ui.min.js"></script>
+
+        <script src="js/index.js"></script>
+
+
+
 
     </body>
 
